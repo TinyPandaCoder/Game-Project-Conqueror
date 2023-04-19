5fc17b3c31711db966b60d25b99d2f06e70ebd21
TinyPandaCoder <asticalaa1@gmail.com>
2023-04-19 17:16:38 +0200

    We added Load Army method

--- src/main/java/com/mycompany/conquer/Conquer.java
+++ src/main/java/com/mycompany/conquer/Conquer.java
@@ -1,9 +1,21 @@
 
 package com.mycompany.conquer;
 
+import java.util.ArrayList;
+
 public class Conquer {
 
     public static void main(String[] args)  {
+ArrayList<Integer> numbers = new ArrayList<>();
+numbers.add(1);
+numbers.add(2);
+numbers.add(3);
+
+for (Integer number : numbers) {
+    number = number + 1;
+}
+
+System.out.println(numbers);
 
     }
 }
--- src/main/java/engine/Game.java
+++ src/main/java/engine/Game.java
@@ -1,9 +1,12 @@
 package engine;
-
 import java.io.IOException;
-import java.util.ArrayList;
-
+import java.util.ArrayList; 
+import units.*;
 public class Game {
+        private final double [][]levelValuesArcher={{60,0.4,0.5,0.6},{60,0.4,0.5,0.6},{70,0.5,0.6,0.7}};
+        private final double [][]levelValuesInfanrty={{50,0.5,0.6,0.7},{50,0.5,0.6,0.7},{60,0.6,0.7,0.8}};
+        private final double [][]levelValuesCavalry={{40,0.6,0.7,0.75},{40,0.6,0.6,0.75},{60,0.7,0.8,0.9}};
+
 	private Player player;
 	private ArrayList<City> availableCities;
 	private ArrayList<Distance> distances;
@@ -11,7 +14,7 @@ public class Game {
 	private int currentTurnCount = 1;
 	public Game(String playerName,String playerCity) throws IOException{
 		this.player = new Player(playerName);
-		City city = new City(playerCity);
+                loadArmy(playerCity);
                 loadCitiesAndDistances();
 	}
         private void loadCitiesAndDistances() throws IOException
@@ -33,7 +36,46 @@ public class Game {
                }   
            }
         }
-
+        public void loadArmy(String cityName) throws IOException
+        {
+            for ( int i=0;i<availableCities.size();i++ )
+            {
+                City city=availableCities.get(i);
+                if(city.getName().compareTo(cityName)!= 0) {
+                    Army army=new Army(city.getName());
+                    ArrayList<String[]> cityUnit= ReadingCSVFile.readFile("./Resources/"+city.getName()+".csv");
+                    ArrayList<Unit>cityUnits=new ArrayList<Unit>();
+                    for (String [] unit : cityUnit)
+                    {
+                        
+                        String type=unit[0];
+                        int level=Integer.parseInt(unit[1]);
+                        switch (type) {
+                            case "Archer":
+                                Archer archer;
+                                archer = new Archer(level,(int)levelValuesArcher[level-1][0],levelValuesArcher[level-1][1],levelValuesArcher[level-1][2],levelValuesArcher[level-1][3]);
+                                cityUnits.add(archer);
+                                break;
+                            case "Cavalry":
+                                Cavalry cavalry;
+                                cavalry = new Cavalry(level,(int)levelValuesCavalry[level-1][0],levelValuesCavalry[level-1][1],levelValuesCavalry[level-1][2],levelValuesCavalry[level-1][3]);
+                                cityUnits.add(cavalry);
+                                break;
+                            case "Infantry":
+                                Infantry infantry;
+                                infantry = new Infantry(level,(int)levelValuesInfanrty[level-1][0],levelValuesInfanrty[level-1][1],levelValuesInfanrty[level-1][2],levelValuesInfanrty[level-1][3]);
+                                cityUnits.add(infantry);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    army.setUnits(cityUnits);
+                    city.setDefendingArmy(army);
+                }
+                availableCities.set(i,city);
+            }
+        }
 	public Player getPlayer() {
 		return player;
 	}
