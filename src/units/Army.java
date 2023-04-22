4b1c178bffeb72d40189a78467bb8c4394bfbde5
TinyPandaCoder <asticalaa1@gmail.com>
2023-04-22 18:39:53 +0200

    I added food needed and relocate unit on army class

--- src/units/Army.java
+++ src/units/Army.java
@@ -1,9 +1,10 @@
 package units;
 
+import exceptions.MaxCapacityException;
 import java.util.ArrayList;
 
 public class Army {
-	private Status currrentStatus = Status.IDLE;
+	private Status currentStatus = Status.IDLE;
 	private ArrayList<Unit> units;
 	private int distancetoTarget = -1;
 	private String target = "";
@@ -13,10 +14,10 @@ public class Army {
 		this.currentLocation = currentLocation;
 	}
 	public Status getCurrrentStatus() {
-		return currrentStatus;
+		return currentStatus;
 	}
 	public void setCurrrentStatus(Status currrentStatus) {
-		this.currrentStatus = currrentStatus;
+		this.currentStatus = currrentStatus;
 	}
 	public ArrayList<Unit> getUnits() {
 		return units;
@@ -49,12 +50,40 @@ public class Army {
         {
             if (unit.getCurrentSoldierCount()<=0)
             {
-                 ArrayList<Unit> newUnits=new ArrayList<Unit>();
-                 newUnits=getUnits();
-                 newUnits.remove((unit));
-                 setUnits(newUnits);
+                this.getUnits().remove(unit);
             }
         }
+        public void relocateUnit(Unit unit) throws MaxCapacityException
+        {
+            if (this.getUnits().size()>=maxToHold)
+            {
+                throw new MaxCapacityException("This Army has the max capcity of units.\nYou can't move the unit to this army");
+            }
+            else
+            {
+                this.getUnits().add(unit);
+                unit.getParentArmy().getUnits().remove(unit);
+                unit.setParentArmy(this);
+            }
+        }
+        public double foodNeeded()
+        {
+            double foodNeeded=0;
+            for (Unit unit : this.getUnits())
+            {
+                foodNeeded+=calcluateFood(unit);
+            }
+            return foodNeeded;
+        }
+        private double calcluateFood(Unit unit) {
+            if (this.currentStatus.equals(Status.IDLE)) {
+              return unit.getCurrentSoldierCount() * unit.getIdleUpkeep();
+            }
+            if (this.currentStatus.equals(Status.BESIEGING)) {
+              return unit.getCurrentSoldierCount() * unit.getSiegeUpkeep();
+            }
+            return unit.getCurrentSoldierCount() * unit.getMarchingUpkeep();
+         }
 
 
 }
